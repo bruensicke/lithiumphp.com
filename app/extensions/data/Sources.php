787baef036037af9a5abab85f873c57f2b3bd522
d1rk <d1rk@gmx.de>
2013-10-27 02:32:16 +0200

    update Sources handling

--- app/extensions/data/Sources.php
+++ app/extensions/data/Sources.php
@@ -1,31 +1,105 @@
 <?php
 /**
- * Lithiumphp: the most rad php framework
+ * Lithiumphp: central hub for lithium development
  *
  * @copyright     Copyright 2013, brünsicke.com GmbH (http://bruensicke.com)
  */
 
 namespace app\extensions\data;
 
+use app\models\Files as FilesModel;
+use app\models\Sources as SourcesModel;
+
 use radium\util\Neon;
 use lithium\core\Libraries;
 
+use FilesystemIterator;
+use DirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+
 class Sources {
 
+	/**
+	 * defines, where to search for source-files
+	 *
+	 * @var string
+	 */
 	public static $path = '/sources';
 
-	public static function find() {
-		$files = static::files();
-		return array_map(function($file) {
-			return basename($file);
-		}, $files);
+	/**
+	 * returns content of filename for a given source
+	 *
+	 * @param string $name name of source to retrieve file content for
+	 * @param string $file filename to retrieve content for, relative to source root
+	 * @return string content of requested file
+	 */
+	public static function file($name, $file) {
+		$folder = static::folder($name);
+		if ($folder === false) {
+			return false;
+		}
+
+		return file_get_contents(sprintf('%s/%s', $folder, $file));
 	}
 
-	public static function files() {
-		return glob(sprintf('%s/*', dirname(LITHIUM_APP_PATH) . static::$path));
+	/**
+	 * returns flat array of files, filtered by given extension (if given).
+	 *
+	 * @param string $name name of source to retrieve files for
+	 * @param string|array $extension file-extensions to be returned
+	 * @param array $options an array with additional options
+	 *        - `nested`: if set to true, will return a nested array, instead of a flat one
+	 * @return array an array containing strings with each relative path and filenames
+	 */
+	public static function files($name, $extension = null, array $options = array()) {
+		$config = static::get($name);
+		$defaults = array('nested' => false, 'subfolder' => $config['path']);
+		$options += $defaults;
+		$folder = static::folder($name, $options['subfolder']);
+
+		if ($folder === false) {
+			return array();
+		}
+
+		$files = array();
+
+		$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
+		$files = array();
+		foreach ($it as $file) {
+
+			if (!in_array($file->getExtension(), (array) $extension)) {
+				continue;
+			}
+			if ($options['nested']) {
+				$path = $file->isDir()
+					? array($file->getFilename() => array())
+					: array($file->getFilename());
+
+				for ($depth = $it->getDepth() - 1; $depth >= 0; $depth--) {
+					$path = array($it->getSubIterator($depth)->current()->getFilename() => $path);
+				}
+				$files = array_merge_recursive($files, $path);
+			} else {
+				if (!$file->isFile()) {
+					continue;
+				}
+				$files[] = str_replace($folder, '', $file);
+			}
+
+		}
+		// var_dump($files);exit;
+		return $files;
 	}
 
+	/**
+	 * returns configuration for a source, as configured in source-file
+	 *
+	 * @param string $name name of source to retrieve configuration for
+	 * @return array all configuration data as associative array
+	 */
 	public static function get($name) {
+		$defaults = array('name' => $name, 'path' => '/', 'type' => 'documentation');
 		$path = dirname(LITHIUM_APP_PATH) . static::$path;
 		$file = sprintf('%s/%s', $path, $name);
 		if (!file_exists($file) || !is_readable($file)) {
@@ -40,33 +114,74 @@ class Sources {
 			return array(
 				'name' => $name,
 				'url' => $data,
+				'path' => '/',
+				'type' => 'documentation',
 			);
 		}
 		if (!is_array($data)) {
 			return false;
 		}
-		if (!isset($data['name'])) {
-			$data['name'] = $name;
-		}
+		$data += $defaults;
 		return $data;
 	}
 
-	public static function sync($name) {
-		return static::updateCopy($name);
-	}
-
-	public static function updateCopy($name) {
+	/**
+	 * returns folder for given source
+	 *
+	 * @param string $name name of source to retrieve folder for
+	 * @return string the absolute path to the folder where cached source-files reside
+	 */
+	public static function folder($name, $subfolder = '/') {
 		$config = static::get($name);
 		if ($config === false) {
 			return false;
 		}
 		$resources = Libraries::get(true, 'resources');
-		$destination = sprintf('%s/tmp/repositories/%s', $resources, $config['name']);
-		$log = sprintf('%s/tmp/logs/%s.log', $resources, $config['name']);
-		$cmd = (is_dir($destination))
-			// ? sprintf('cd %s && git clean && git pull >> %s 2>&1', $destination, $log)
-			? sprintf('cd %s && git fetch origin && git reset --hard origin/master >> %s 2>&1', $destination, $log)
-			: sprintf('git clone %s %s >> %s 2>&1', $config['url'], $destination, $log);
+		$path = sprintf('%s/tmp/repositories/%s', $resources, $name);
+		if ($subfolder === true) {
+			$subfolder = $config['path'];
+		}
+		if ($subfolder == '/') {
+			return $path;
+		}
+		return $path.$subfolder;
+	}
+
+	/**
+	 * syncs the local copy of all files with remote repository and updates all
+	 * local data in the database to reflect latest changes.
+	 *
+	 * @param string $name name of source to sync files and data for
+	 * @return boolean true on success, false otherwise
+	 */
+	public static function sync($name) {
+		$result = static::updateCopy($name);
+		if ($result === false) {
+			return false;
+		}
+		// $result = static::generateApidocs($name);
+		// if ($result === false) {
+		// 	return false;
+		// }
+		$result = static::importData($name);
+		if ($result === false) {
+			return false;
+		}
+		return true;
+	}
+
+	public static function generateApidocs($name) {
+		$config = static::get($name);
+		$folder = static::folder($name);
+		$resources = Libraries::get(true, 'resources');
+		$path = Libraries::get(true, 'path');
+		$destination = sprintf('%s/webroot/api/%s', $path, $name);
+		$log = sprintf('%s/tmp/logs/%s.log', $resources, $name);
+		if (!is_dir($folder)) {
+			return false;
+		}
+		// $cmd = sprintf('apigen -s %s -d %s >> %s 2>&1', $folder, $destination, $log);
+		$cmd = sprintf('phpdoc -d %s -t %s >> %s 2>&1', $folder, $destination, $log);
 		$output = array();
 		$code = null;
 		exec($cmd, $output, $code);
@@ -76,6 +191,115 @@ class Sources {
 		return false;
 	}
 
+	/**
+	 * imports all relevant files into database
+	 *
+	 * @param string $name name of source to import file data for
+	 * @return boolean true on success, false otherwise
+	 */
+	public static function importData($name) {
+		$config = static::get($name);
+		$source = SourcesModel::load($name);
+		if (!$source) {
+			$source = SourcesModel::create();
+			$source->slug = $name;
+		}
+		$data = array(
+			'name' => $source->name,
+			'description' => $source->description,
+			'path' => $source->path,
+			'url' => $source->url,
+		);
+		$diff = array_diff($data, $config);
+		if (!empty($diff)) {
+			$source->set($config);
+			$source->save();
+		}
+
+		$docs = static::files($name, array('md', 'markdown', 'php'));
+		if ($docs === false) {
+			return false;
+		}
+		if (empty($docs)) {
+			return true;
+		}
+		foreach ($docs as $doc) {
+			$id = sha1($name.$doc);
+			$file = FilesModel::first($id);
+			if (!$file) {
+				$file = FilesModel::create();
+				$file->source = $name;
+				$data = array(
+					'name' => $doc,
+					'type' => (pathinfo($doc, PATHINFO_EXTENSION) == 'php') ? 'php' : 'markdown',
+					'extension' => pathinfo($doc, PATHINFO_EXTENSION),
+					'path' => dirname($doc),
+					'filename' => basename($doc),
+					// 'content' => static::file($doc),
+				);
+				$file->set($data);
+				$file->save();
+			}
+			// echo $doc;
+		}
+		return true;
+	}
+
+	/**
+	 * updates or creates local cached copy of remote repository
+	 *
+	 * @param string $name name of source to create/update local cache for
+	 * @return boolean true on success, false otherwise
+	 */
+	public static function updateCopy($name) {
+		$config = static::get($name);
+		if ($config === false) {
+			return false;
+		}
+		$destination = static::folder($name);
+		$resources = Libraries::get(true, 'resources');
+		$log = sprintf('%s/tmp/logs/%s.log', $resources, $name);
+		$cmd = (is_dir($destination))
+		/* ? sprintf('cd %s && git clean && git pull >> %s 2>&1', $destination, $log) */
+		 ? sprintf('{ cd %s && git fetch origin && git reset --hard origin/master; } >> %s 2>&1'
+			, $destination, $log)
+		 : sprintf('git clone --recursive %s %s >> %s 2>&1', $config['url'], $destination, $log);
+		$output = array();
+		$code = null;
+		exec($cmd, $output, $code);
+		if ($code === 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * returns all available sources
+	 *
+	 * @see  Sources::all()
+	 * @return array an array containing all source-files available
+	 */
+	public static function find() {
+		return static::all();
+	}
+
+	/**
+	 * returns all available sources
+	 *
+	 * @see  Sources::find()
+	 * @return array an array containing all source-files available
+	 */
+	public static function all() {
+		$folder = dirname(LITHIUM_APP_PATH) . static::$path;
+		$it = new DirectoryIterator($folder);
+		$files = array();
+		foreach ($it as $file) {
+			if (!$it->isDot()) {
+				$files[] = $file->getFilename();
+			}
+		}
+		return $files;
+	}
 }
 
 ?>
--- app/models/Sources.php
+++ app/models/Sources.php
@@ -2,7 +2,116 @@
 
 namespace app\models;
 
+use lithium\util\Set;
+
+use app\extensions\data\Sources as CoreSources;
+
 class Sources extends BaseModel {
+
+	/**
+	 * Custom type options
+	 *
+	 * @var array
+	 */
+	public static $_types = array(
+		'documentation' => 'documentation',
+		'library' => 'library',
+	);
+
+	/**
+	 * Stores the data schema.
+	 *
+	 * @see lithium\data\source\MongoDb::$_schema
+	 * @var array
+	 */
+	protected $_schema = array(
+		'type' => array('type' => 'string', 'default' => 'documentation'),
+		'url' => array('type' => 'string', 'null' => false),
+		'path' => array('type' => 'string', 'default' => '/', 'null' => false),
+		'description' => array('type' => 'string', 'null' => true),
+	);
+
+	/**
+	 * Custom find query properties, indexed by name.
+	 *
+	 * @var array
+	 */
+	public $_finders = array(
+		'manual' => array(
+			'conditions' => array(
+				'type' => 'manual',
+			)
+		),
+		'docs' => array(
+			'conditions' => array(
+				'type' => 'documentation',
+			)
+		),
+		'libraries' => array(
+			'conditions' => array(
+				'type' => 'library',
+			)
+		),
+	);
+
+	/**
+	 * Criteria for data validation.
+	 *
+	 * @see lithium\data\Model::$validates
+	 * @see lithium\util\Validator::check()
+	 * @var array
+	 */
+	public $validates = array(
+		'slug' => array(
+			array('notEmpty', 'severity' => 'important', 'message' => 'Source must have a slug.'),
+			array('slug', 'severity' => 'important', 'message' => 'Source slug must be valid.'),
+		),
+	);
+
+	/**
+	 * Default query parameters.
+	 *
+	 * @var array
+	 */
+	protected $_query = array(
+		'order' => array(
+			'order' => 'ASC',
+			'name' => 'ASC',
+		),
+	);
+
+	/**
+	 * return files for that source.
+	 *
+	 * Paths are relative, starting from root of source.
+	 *
+	 * @see app\extensions\data\Sources
+	 * @param object $entity current object instance
+	 * @param string|array $extension only return files with these extensions
+	 * @param array $options additional options
+	 *        - `raw`: set to true, to return files directly via filesystem from repository-clone
+	 *        - `nested`: set to true, to return files with a nested structure
+	 * @return array returns all files as array
+	 */
+	public function files($entity, $extension = null, array $options = array()) {
+		$defaults = array('raw' => false, 'nested' => false);
+		$options += $defaults;
+		if ($options['raw']) {
+			return CoreSources::files($entity->slug, $extension, $options);
+		}
+
+		$conditions = array(
+			'source' => $entity->slug,
+			'extension' => $extension,
+		);
+		$files = Files::find('all', compact('conditions'));
+		if (!$options['nested']) {
+			return $files;
+		}
+		$files = $files->map(function($f){ return $f->name; }, array('collect' => false));
+		$files = Sources::explodeTree($files);
+		return $files;
+	}
 
 }
 
